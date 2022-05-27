ea0768381c70e79005e47e53d9e66aa8713a5fd4
Bob Merullo <bmerullo85@gmail.com>
2022-05-27 10:08:39 -0600

    Added wireframe file.  Added Main Pages

---
+++ koholint/src/pages/catagories.js
@@ -0,0 +1,11 @@
+import React from "react"
+
+const catagories = () => {
+  return (
+    <div>
+      <h2>This is the catagories page</h2>
+    </div>
+  )
+}
+
+export default catagories
---
+++ koholint/src/pages/explore.js
@@ -0,0 +1,11 @@
+import React from "react"
+
+const explore = () => {
+  return (
+    <div>
+      <h2>This is the explore page</h2>
+    </div>
+  )
+}
+
+export default explore
