f51eee4c7853c676bfdcaf70e3bcf32086174715
Bob Merullo <bmerullo85@gmail.com>
2022-07-01 14:56:35 -0600

    added ternary argument for location list

--- koholint/src/pages/{ContentfulLinksAwakening.name}.js
+++ koholint/src/pages/{ContentfulLinksAwakening.name}.js
@@ -1,12 +1,51 @@
 import React from "react"
+import { graphql } from "gatsby"
+import { gatsbyImage, getImage } from "gatsby-plugin-image"
+import Layout from "../components/Layout"
 
-const ItemTemplate = props => {
-  console.log(props)
+const ItemTemplate = ({ data }) => {
+  const {
+    name,
+    description: { description },
+    image,
+    info: { locations, catagories },
+    locationBoolean,
+  } = data.contentfulLinksAwakening
+  const items = data.contentfulLinksAwakening
+
+  // console.log(data)
   return (
-    <div>
-      <h1>{props.params.name}</h1>
-    </div>
+    <Layout>
+      <main className="page">
+        <div>
+          <h4>{name}</h4>
+          <p>{description}</p>
+          <p>Catagories: {catagories}</p>
+        </div>
+        {locationBoolean === true ? <div>test</div> : null}
+      </main>
+    </Layout>
   )
 }
+
+export const query = graphql`
+  query getSingleItem($name: String) {
+    contentfulLinksAwakening(name: { eq: $name }) {
+      name
+      description {
+        description
+      }
+      id
+      image {
+        gatsbyImageData(layout: FIXED, placeholder: BLURRED)
+      }
+      info {
+        locations
+        catagories
+      }
+      locationBoolean
+    }
+  }
+`
 
 export default ItemTemplate
